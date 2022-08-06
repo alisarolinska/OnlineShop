db7f3f563c9260132f669ac1776a1019bbdcc093
alisarolinska <102417863+alisarolinska@users.noreply.github.com>
2022-08-06 18:24:15 +0300

    commit8 final

--- shop/urls.py
+++ shop/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('orders/<int:product_id>', views.order_create, name='order_create'),
     path('category/<int:category>', views.get_products_by_category, name='category'),
     path('detail/<int:id>', views.product_detail_view, name='product-detail')
-	# path('путь', views.функция, имя_запроса)
+
 ]
